dd98e11b8ffe9f0c9189a2ef6d2779440b387fab
HOSEIN\Hosein <eshrati@outlook.com>
2016-11-17 12:35:45 +0330

    end

--- Practic72/app/src/main/java/com/example/hosein/practic7/MainActivity.java
+++ Practic72/app/src/main/java/com/example/hosein/practic7/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
                 name = (EditText) findViewById(R.id.editname);
                 family = (EditText) findViewById(R.id.editfamily);
@@ -31,13 +34,25 @@ public class MainActivity extends AppCompatActivity {
                 String family1 = family.getText().toString();
                 String emil1 = email.getText().toString();
                 int age1 = Integer.parseInt(age.getText().toString());
-                intent.putExtra("namego", name1);
-                intent.putExtra("familygo", family1);
-                intent.putExtra("emailgo", emil1);
-                intent.putExtra("agego", age1);
-                startActivity(intent);
-            }
-        });
+                if (!name1.equals("") && !family1.equals("") && !emil1.equals("") && age1 <= 100 && age1 > 5) {
+                    intent.putExtra("namego", name1);
+                    intent.putExtra("familygo", family1);
+                    intent.putExtra("emailgo", emil1);
+                    intent.putExtra("agego", age1);
+                    startActivity(intent);
+                }
+                else {
 
+                    Toast.makeText(MainActivity.this, "Please Check your information input correctly ", Toast.LENGTH_SHORT).show();
+
+                }
+
+
+                }
+
+            }
+
+            );
+
+        }
     }
-}
